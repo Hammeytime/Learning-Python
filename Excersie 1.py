c0bd5025157c9579eed57a4ee4312faf5e135993
Hammeytime <hamboandedana@gmail.com>
2025-01-22 14:12:33 -0800

    Python Exercises

---
+++ Excersie 1.py
@@ -0,0 +1,4 @@
+name = "John Smith"
+Age = 20
+Type = 'New Patient'
+print (name,Age,type)
---
+++ input 2.py
@@ -0,0 +1,6 @@
+birth_year = input ("Enter your birth year: ")
+
+age = 2025 - int(birth_year)
+print (age)
+
+float()
